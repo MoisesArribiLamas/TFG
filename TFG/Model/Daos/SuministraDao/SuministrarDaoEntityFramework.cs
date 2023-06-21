e916c5dee2077c9c63fad4adb8b386823f0bb8db
MoisesArribiLamas <moises.arribi@udc.es>
2023-06-21 17:23:34 +0200

    CargaDao Buscar suministros por su id y tests

---
+++ TFG/Model/Daos/SuministraDao/SuministrarDaoEntityFramework.cs
@@ -0,0 +1,26 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es.Udc.DotNet.TFG.Model.Daos.SuministraDao
+{
+    public class SuministraDaoEntitFramework : GenericDaoEntityFramework<Suministra, Int64>, ISuministraDao
+    {
+        
+        public Suministra getInfoSuministra(long suministraId)
+        {
+            DbSet<Suministra> suministraciones = Context.Set<Suministra>();
+
+            var result =
+                (from s in suministraciones
+                 where s.suministraId == suministraId
+                 select s).FirstOrDefault();
+
+            return result;
+        }
+    }
+}
--- TFG/Test/TestManager.cs
+++ TFG/Test/TestManager.cs
@@ -14,6 +14,7 @@ using Es.Udc.DotNet.TFG.Model.Daos.TarifaDao;
 using Es.Udc.DotNet.TFG.Model.Daos.BateriaDao;
 using Es.Udc.DotNet.TFG.Model.Daos.EstadoBateriaDao;
 using Es.Udc.DotNet.TFG.Model.Daos.CargaDao;
+using Es.Udc.DotNet.TFG.Model.Daos.SuministraDao;
 
 namespace Es.Udc.DotNet.TFG.Test
 {
@@ -37,6 +38,7 @@ namespace Es.Udc.DotNet.TFG.Test
             kernel.Bind<IBateriaDao>().To<BateriaEntityFramework>(); 
             kernel.Bind<IEstadoBateriaDao>().To<EstadoBateriaDaoEntitFramework>();
             kernel.Bind<ICargaDao>().To<CargaDaoEntitFramework>();
+            kernel.Bind<ISuministraDao>().To<SuministraDaoEntitFramework>();
 
 
 
