c09a437983542ff238ae13394d961fb5ddc70c65
MoisesArribiLamas <moises.arribi@udc.es>
2023-07-25 22:17:45 +0200

    servicio de mostrar tarifas de un dia

---
+++ TFG/Model/Service/Tarifa/IServiceTarifa.cs
@@ -0,0 +1,19 @@
+﻿using Es.Udc.DotNet.ModelUtil.Transactions;
+using Es.Udc.DotNet.TFG.Model.Dao.UsuarioDao;
+using Es.Udc.DotNet.TFG.Model.Daos.EstadoDao;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Es.Udc.DotNet.TFG.Model.Service.Tarifas
+{
+    public interface IServiceTarifa
+    {
+
+        [Transactional]
+        List<TarifaDTO> verTarifasDelDia(DateTime fecha);
+
+    }
+}
---
+++ TFG/Model/Service/Tarifa/ServiceTarifa.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Es.Udc.DotNet.ModelUtil.Exceptions;
+using Es.Udc.DotNet.ModelUtil.Transactions;
+using Es.Udc.DotNet.TFG.Model.Dao.UsuarioDao;
+using Es.Udc.DotNet.TFG.Model.Daos.EstadoDao;
+using Es.Udc.DotNet.TFG.Model.Daos.TarifaDao;
+using Es.Udc.DotNet.TFG.Model.Daos.UbicacionDao;
+using Es.Udc.DotNet.TFG.Model.Service.Estados;
+using Es.Udc.DotNet.TFG.Model.Service.Tarifas;
+using Ninject;
+
+namespace Es.Udc.DotNet.TFG.Model.Service.Estados
+{
+    public class ServiceTarifa : IServiceTarifa
+    {
+
+        [Inject]
+        public ITarifaDao tarifaDao { private get; set; }
+
+
+        #region mostrar las tarifas del dia
+        [Transactional]
+        public List<TarifaDTO> verTarifasDelDia(DateTime fecha)
+        {
+            try
+            {
+                List<TarifaDTO> tarifasDTO = new List<TarifaDTO>();
+
+                List<Tarifa> tarifas = tarifaDao.verTarifasDelDia(fecha);
+
+                foreach (Tarifa t in tarifas)
+                {
+                    tarifasDTO.Add(new TarifaDTO(t.precio, t.hora, t.fecha));
+                }
+                return tarifasDTO;
+
+            }
+            catch (InstanceNotFoundException)
+            {
+                return null;
+            }
+        } /*
+        public List<UbicacionProfileDetails> verUbicaciones(long idUsuario, int startIndex, int count)
+        {
+            try
+            {
+                List<UbicacionProfileDetails> ubicacionesDTO = new List<UbicacionProfileDetails>();
+
+                List<Ubicacion> ubicaciones = ubicacionDao.ubicacionesUsuario(idUsuario, startIndex, count);
+
+                foreach (Ubicacion u in ubicaciones)
+                {
+                    ubicacionesDTO.Add(new UbicacionProfileDetails(u.codigoPostal, u.localidad, u.calle, u.portal, u.numero));
+                }
+                return ubicacionesDTO;
+
+            }
+            catch (InstanceNotFoundException)
+            {
+                return null;
+            }
+         */
+
+        #endregion
+
+
+    }
+
+}
