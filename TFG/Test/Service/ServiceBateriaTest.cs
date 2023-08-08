3db27e0ca312809dff11146ba468bca6d74557ec
MoisesArribiLamas <moises.arribi@udc.es>
2023-08-08 10:29:57 +0200

    test buscar carga por id (servicio)

--- TFG/Test/Service/ServiceBateriaTest.cs
+++ TFG/Test/Service/ServiceBateriaTest.cs
@@ -462,6 +462,49 @@ namespace Es.Udc.DotNet.TFG.Model.Service.Tests
 
             }
         }
+        /*
+        [TestMethod()]
+        public void InfoCargaTest()
+        {
+            using (var scope = new TransactionScope())
+            {
 
+                long usuarioId = crearUsuario(nombre, email, apellido1, apellido2, contraseña, telefono, pais, idioma);
+                long ubicacionId = crearUbicacion(codigoPostal, localidad, calle, portal, numero);
+
+                //Creamos Bateria
+                long bateriaId = servicio.CrearBateria(ubicacionId, usuarioId, precioMedio, kwAlmacenados, almacenajeMaximoKw,
+             fechaDeAdquisicion, marca, modelo, ratioCarga, ratioCompra, ratioUso);
+
+                //Creamos Tarifa
+                DateTime fecha = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+                long tarifaId = crearTarifa(500, 0, fecha);
+
+                //creamos Carga
+                int hour1 = 1;
+                int hour2 = 2;
+                int minutes = 0;
+                int seconds = 0;
+                TimeSpan horaIni = new TimeSpan(hour1, minutes, seconds);
+                TimeSpan horaFin = new TimeSpan(hour2, minutes, seconds);
+                double kws = 3000;
+
+                long cargaId = servicio.CrearCarga(bateriaId, tarifaId, horaIni, horaFin, kws);
+
+                //Buscamos
+                var c = servicio.ge
+
+
+                //Comprobamos
+
+                Assert.AreEqual(bateriaId, c.bateriaId);
+                Assert.AreEqual(tarifaId, c.tarifaId);
+                Assert.AreEqual(horaIni, c.horaIni);
+                Assert.AreEqual(horaFin, c.horaFin);
+                Assert.AreEqual(kws, c.kws);
+
+
+            }
+        }*/
     }
 }
