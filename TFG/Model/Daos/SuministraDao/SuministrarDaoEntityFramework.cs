737f12c764fc3f74d82a7870cc397129c8e4e324
MoisesArribiLamas <moises.arribi@udc.es>
2023-08-09 19:42:52 +0200

    Ahorro de las baterias de un usuario en un periodo (Dao)

--- TFG/Model/Daos/SuministraDao/SuministrarDaoEntityFramework.cs
+++ TFG/Model/Daos/SuministraDao/SuministrarDaoEntityFramework.cs
@@ -37,7 +37,7 @@ namespace Es.Udc.DotNet.TFG.Model.Daos.SuministraDao
         }
         #endregion
 
-        #region ahorro entre fechas
+        #region ahorro bateria entre fechas
         public double ahorroBareriaPorFecha(long bateriaId, DateTime fecha, DateTime fecha2)
         {
             DbSet<Suministra> suministros = Context.Set<Suministra>();
@@ -46,6 +46,23 @@ namespace Es.Udc.DotNet.TFG.Model.Daos.SuministraDao
             var result =
                 (from s in suministros
                  where ((s.Tarifa.fecha >= fecha) && (s.Tarifa.fecha <= fecha2) && (s.bateriaId == bateriaId))
+                 select s).ToList();
+
+            double Montofinal = MontoInical + result.Sum(x => x.ahorro);
+
+            return Montofinal;
+        }
+        #endregion
+
+        #region ahorro usuario entre fechas
+        public double ahorroUsuarioPorFecha(long usuarioId, DateTime fecha, DateTime fecha2)
+        {
+            DbSet<Suministra> suministros = Context.Set<Suministra>();
+            double MontoInical = 0;
+
+            var result =
+                (from s in suministros
+                 where ((s.Tarifa.fecha >= fecha) && (s.Tarifa.fecha <= fecha2) && (s.Bateria.usuarioId == usuarioId))
                  select s).ToList();
 
             double Montofinal = MontoInical + result.Sum(x => x.ahorro);
