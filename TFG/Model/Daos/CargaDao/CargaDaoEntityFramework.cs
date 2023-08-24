7940ab4afb54fd45a273df543e810116f2bea0a7
MoisesArribiLamas <moises.arribi@udc.es>
2023-08-24 20:17:34 +0200

    Resuelto el bug de buscar ultima carga

--- TFG/Model/Daos/CargaDao/CargaDaoEntityFramework.cs
+++ TFG/Model/Daos/CargaDao/CargaDaoEntityFramework.cs
@@ -48,7 +48,9 @@ namespace Es.Udc.DotNet.TFG.Model.Daos.CargaDao
             var result =
                 (from c in Cargas
                  where c.bateriaId == bateriaId
-                 select c).OrderByDescending(c => c.horaIni).FirstOrDefault();
+                 select c).OrderByDescending(c => c.Tarifa.fecha).ThenByDescending(c => c.horaIni).FirstOrDefault();
+            //
+            //select c).OrderByDescending(c => c.horaIni).FirstOrDefault();
 
             return result;
         }
