25bea96ea5bb090578a690dc1efcd7ea3ec31ac7
MoisesArribiLamas <moises.arribi@udc.es>
2024-06-18 13:08:58 +0200

    mostrar ahorro por dias

---
+++ TFG/Web/Pages/Graficas/AhorroXDias.aspx.cs
@@ -0,0 +1,275 @@
+﻿using Es.Udc.DotNet.ModelUtil.IoC;
+using Es.Udc.DotNet.TFG.Model;
+using Es.Udc.DotNet.TFG.Model.Daos.AhorroDao;
+using Es.Udc.DotNet.TFG.Model.Daos.ConsumoDao;
+using Es.Udc.DotNet.TFG.Model.Service.Baterias;
+using Es.Udc.DotNet.TFG.Model.Service.Ubicaciones;
+using Es.Udc.DotNet.TFG.Web.HTTP.Session;
+using Es.Udc.DotNet.TFG.Web.HTTP.View.ApplicationObjects;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Es.Udc.DotNet.TFG.Web.Pages.Graficas
+{
+    public partial class AhorroXDias : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //Obtenemos parametro
+                String ubicacionId = Request.Params.Get("idUbicacion");
+                txtFecha.Text = Request.Params.Get("fechaIni");
+                txtFecha2.Text = Request.Params.Get("fechaFin");
+                ddlListaCriterios.Text = Request.Params.Get("criterio");
+
+
+                // obtenemos el servicio Ubicacion
+                IIoCManager iocManager = (IIoCManager)HttpContext.Current.Application["managerIoC"];
+                IServiceUbicacion serviceUbicacion = iocManager.Resolve<IServiceUbicacion>();
+
+                Ubicacion ubicacion = serviceUbicacion.buscarUbicacionById(Convert.ToInt64(ubicacionId));
+
+                // ponemos el nombre de la ubicacion
+                lblNombreUbicacion.Text = ubicacion.etiqueta;
+
+                // ocultamos los calendarios
+                Calendar1.Visible = false;
+                Calendar2.Visible = false;
+
+                // Desplegable con los criterios
+                string idioma = SessionManager.GetUserSession(Context).Idioma;
+                this.ddlListaCriterios.DataSource = Statistics.GetStatistics(idioma);
+                this.ddlListaCriterios.DataBind();
+            }
+        }
+
+        protected void btnCalendario_Click(object sender, EventArgs e)
+        {
+            // Muestra u oculta el calendario
+            Calendar1.Visible = !Calendar1.Visible;
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            txtFecha.Text = Calendar1.SelectedDate.ToShortDateString();
+            Calendar1.Visible = !Calendar1.Visible;
+
+        }
+
+        protected void ddlListaCriterios_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Obtenemos parametro
+            String ubicacionId = Request.Params.Get("idUbicacion");
+
+
+
+            //SuministradoXRed
+            if (ddlListaCriterios.Text == "Supplied by the Network" || ddlListaCriterios.Text == "Suministrado por la Red")
+            {
+                String url = String.Format("~/Pages/Graficas/SuministradoXRed.aspx?idUbicacion={0}" +
+                     "&fechaIni={1}" + "&fechaFin={2}" + "&criterio={3}", ubicacionId, txtFecha.Text, txtFecha2.Text, ddlListaCriterios.Text);
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+
+            //SuministradoXBateria
+            if (ddlListaCriterios.Text == "Supplied" || ddlListaCriterios.Text == "Suministrado" || ddlListaCriterios.Text == "Suministrou")
+            {
+                String url = String.Format("~/Pages/Graficas/SuministradoXBateria.aspx?idUbicacion={0}" +
+                     "&fechaIni={1}" + "&fechaFin={2}" + "&criterio={3}", ubicacionId, txtFecha.Text, txtFecha2.Text, ddlListaCriterios.Text);
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+
+            //CargasDelSistema
+            if (ddlListaCriterios.Text == "Loaded" || ddlListaCriterios.Text == "Cargados" || ddlListaCriterios.Text == "Cargou")
+            {
+                String url = String.Format("~/Pages/Graficas/CargasDelSistema.aspx?idUbicacion={0}" +
+                     "&fechaIni={1}" + "&fechaFin={2}" + "&criterio={3}", ubicacionId, txtFecha.Text, txtFecha2.Text, ddlListaCriterios.Text);
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+
+            //AhorroXDias
+            if (ddlListaCriterios.Text == "Saving money" || ddlListaCriterios.Text == "Ahorro" || ddlListaCriterios.Text == "Aforro")
+            {
+                String url = String.Format("~/Pages/Graficas/AhorroXDias.aspx?idUbicacion={0}" +
+                     "&fechaIni={1}" + "&fechaFin={2}" + "&criterio={3}", ubicacionId, txtFecha.Text, txtFecha2.Text, ddlListaCriterios.Text);
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+
+            //CargaSuministraVSRed
+            if (ddlListaCriterios.Text == "Network vs Loaded vs Supplied" || ddlListaCriterios.Text == "Red vs Suministrado Cargados" || ddlListaCriterios.Text == "Red vs Suministrou Cargou")
+            {
+                String url = String.Format("~/Pages/Graficas/CargaSuministraVSRed.aspx?idUbicacion={0}" +
+                     "&fechaIni={1}" + "&fechaFin={2}" + "&criterio={3}", ubicacionId, txtFecha.Text, txtFecha2.Text, ddlListaCriterios.Text);
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+
+        }
+
+        protected void btnBuscar2_Click(object sender, EventArgs e)
+        {
+            // obtenemso el datetime de los calendarios
+
+            if (txtFecha.Text != "")
+            {
+                string format = "dd/mm/yyyy";
+
+                DateTime dateTime = DateTime.ParseExact(txtFecha.Text, format, CultureInfo.InvariantCulture);
+
+            }
+
+            if (txtFecha2.Text != "")
+            {
+                string format = "dd/mm/yyyy";
+
+                DateTime dateTime = DateTime.ParseExact(txtFecha2.Text, format, CultureInfo.InvariantCulture);
+            }
+
+            //Obtenemos parametro
+            String ubicacionId = Request.Params.Get("idUbicacion");
+
+
+
+            //SuministradoXRed
+            if (ddlListaCriterios.Text == "Supplied by the Network" || ddlListaCriterios.Text == "Suministrado por la Red")
+            {
+                String url = String.Format("~/Pages/Graficas/SuministradoXRed.aspx?idUbicacion={0}" +
+                     "&fechaIni={1}" + "&fechaFin={2}" + "&criterio={3}", ubicacionId, txtFecha.Text, txtFecha2.Text, ddlListaCriterios.Text);
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+
+            //SuministradoXBateria
+            if (ddlListaCriterios.Text == "Supplied" || ddlListaCriterios.Text == "Suministrado" || ddlListaCriterios.Text == "Suministrou")
+            {
+                String url = String.Format("~/Pages/Graficas/SuministradoXBateria.aspx?idUbicacion={0}" +
+                     "&fechaIni={1}" + "&fechaFin={2}" + "&criterio={3}", ubicacionId, txtFecha.Text, txtFecha2.Text, ddlListaCriterios.Text);
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+
+            //CargasDelSistema
+            if (ddlListaCriterios.Text == "Loaded" || ddlListaCriterios.Text == "Cargados" || ddlListaCriterios.Text == "Cargou")
+            {
+                String url = String.Format("~/Pages/Graficas/CargasDelSistema.aspx?idUbicacion={0}" +
+                     "&fechaIni={1}" + "&fechaFin={2}" + "&criterio={3}", ubicacionId, txtFecha.Text, txtFecha2.Text, ddlListaCriterios.Text);
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+
+            //AhorroXDias
+            if (ddlListaCriterios.Text == "Saving money" || ddlListaCriterios.Text == "Ahorro" || ddlListaCriterios.Text == "Aforro")
+            {
+                String url = String.Format("~/Pages/Graficas/AhorroXDias.aspx?idUbicacion={0}" +
+                     "&fechaIni={1}" + "&fechaFin={2}" + "&criterio={3}", ubicacionId, txtFecha.Text, txtFecha2.Text, ddlListaCriterios.Text);
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+
+            //CargaSuministraVSRed
+            if (ddlListaCriterios.Text == "Network vs Loaded vs Supplied" || ddlListaCriterios.Text == "Red vs Suministrado Cargados" || ddlListaCriterios.Text == "Red vs Suministrou Cargou")
+            {
+                String url = String.Format("~/Pages/Graficas/CargaSuministraVSRed.aspx?idUbicacion={0}" +
+                     "&fechaIni={1}" + "&fechaFin={2}" + "&criterio={3}", ubicacionId, txtFecha.Text, txtFecha2.Text, ddlListaCriterios.Text);
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+        }
+
+        protected void btnCalendario2_Click(object sender, EventArgs e)
+        {
+            // Muestra u oculta el calendario
+            Calendar2.Visible = !Calendar2.Visible;
+        }
+
+        protected void Calendar2_SelectionChanged(object sender, EventArgs e)
+        {
+            txtFecha2.Text = Calendar2.SelectedDate.ToShortDateString();
+            Calendar2.Visible = !Calendar2.Visible;
+        }
+
+        protected string titulo()
+        {
+
+            string idioma = SessionManager.GetUserSession(Context).Idioma;
+
+            if (idioma == "es") // castellano
+            {
+                return "€ ahorrados";
+
+
+            }
+            else if (idioma == "gl") // gallego
+            {
+                return "€ aforrados";
+            }
+            else // (idioma == "en") ingles
+            {
+                return "Money saved";
+
+            }
+
+        }
+
+
+        protected string ahorroPorDias()
+        {
+
+            //Obtenemos parametro
+            String ubicacionId = Request.Params.Get("idUbicacion");
+
+            // obtenemos el servicio Ubicacion
+            IIoCManager iocManager = (IIoCManager)HttpContext.Current.Application["managerIoC"];
+            IServiceUbicacion serviceUbicacion = iocManager.Resolve<IServiceUbicacion>();
+            IServiceBateria serviceBateria = iocManager.Resolve<IServiceBateria>();
+
+            Ubicacion ubicacion = serviceUbicacion.buscarUbicacionById(Convert.ToInt64(ubicacionId));
+
+            //string format = "dd/mm/yyyy";
+
+            //DateTime fechaIni = DateTime.ParseExact(txtFecha.Text, format, CultureInfo.InvariantCulture).Date;
+
+            //DateTime fechaFin = DateTime.ParseExact(txtFecha2.Text, format, CultureInfo.InvariantCulture).Date;
+
+            DateTime fechaIni = Convert.ToDateTime(txtFecha.Text);
+
+            DateTime fechaFin = Convert.ToDateTime(txtFecha2.Text);
+
+
+            List<AhorroPorDias> ahorros = serviceBateria.MostrarAhorroXUbicacionPorFechaEnDias(ubicacion.ubicacionId, fechaIni, fechaFin);
+
+            string strDatos;
+            double ahorroTotal = 0;
+
+            strDatos = "[['Dia','(€)'],";
+
+            foreach (AhorroPorDias dr in ahorros)
+            {
+                string f = (dr.fecha).ToString();
+                string fecha = f.Substring(0, f.IndexOf(" "));
+
+                ahorroTotal = ahorroTotal + dr.Count;
+
+                string a = dr.Count.ToString();
+                string e = a.Substring(0, a.IndexOf(","));
+
+                int index = a.IndexOf(",") + 1;
+                string d = a.Substring(index);
+                // tenemos que ponerle un punto ya que no funciona con la coma decimal.
+                string ahorro = e + "." + d;
+
+                //}
+                strDatos = strDatos + "[";
+                strDatos = strDatos + "'" + fecha + "'" + "," + ahorro; 
+                strDatos = strDatos + "],";
+            }
+
+            strDatos = strDatos + "]";
+
+            lblahorro.Text = ahorroTotal.ToString();
+
+            return strDatos;
+        }
+    }
+}
